e29d2bfcd320af87f01666e621694833a5a82eb5
clmakihara <clmakihara@gmail.com>
2017-06-12 21:15:50 -0300

    Criaçao do Projeto

---
+++ meusistema/src/main/java/br/univel/Aluno.java
@@ -0,0 +1,35 @@
+package br.univel;
+
+public class Aluno {
+	
+	private long id;
+	private String nome;
+	private int idade;
+	private String telefone;
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
