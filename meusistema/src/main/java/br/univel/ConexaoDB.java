505f4f9b7a49449d247366e0815165fc06b9b045
clmakihara <clmakihara@gmail.com>
2017-06-12 21:44:06 -0300

    Criacao do Dao

---
+++ meusistema/src/main/java/br/univel/ConexaoDB.java
@@ -0,0 +1,49 @@
+package br.univel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConexaoDB {
+
+	private static ConexaoDB self;
+	
+	private Connection con;
+	
+	private ConexaoDB() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			this.con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/alunos","postgres", "univel");
+			
+			Runtime.getRuntime()
+				.addShutdownHook(new Thread(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							ConexaoDB.this.con.close();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
+				}));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+	public final static synchronized ConexaoDB getInstance() {
+		if (self == null) {
+			self = new ConexaoDB();
+		}
+		return self;
+	}
+	
+	public Connection getConnection() {
+		return this.con;
+	}
+
+	
+}
+
---
+++ meusistema/src/main/java/br/univel/instrucao.sql
@@ -0,0 +1,10 @@
+
+
+CREATE DATABASE alunos
+
+create table aluno(
+	id serial,
+	nome varchar(100),
+	idade int,
+	telefone varchar(11)
+)
